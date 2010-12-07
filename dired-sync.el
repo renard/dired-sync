6869342803f32e7fc3dafc6a191963dd452ffc14
Sébastien Gross <sebchezwamorg>
2010-12-07 15:22:16 +0100

    Add autoload to dired-sync.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- dired-sync.el
+++ dired-sync.el
@@ -5,7 +5,7 @@
 ;; Author: Sebastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, dired, rsync
 ;; Created: 2010-12-02
-;; Last changed: 2010-12-07 15:20:36
+;; Last changed: 2010-12-07 15:21:27
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -449,6 +449,7 @@ SOURCE."
      (list :src src :dst dst))))
 
 
+;;;###autoload
 (defun dired-sync (&optional source destination)
   "sync 2 directories using `dired-sync-bin'."
   (interactive)
