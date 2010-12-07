669924d7b0d03cda93c3c384b319c8a7f816d539
Sébastien Gross <sebchezwamorg>
2010-12-07 11:15:30 +0100

    Change dired-sync to use dired-sync-with-files.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- dired-sync.el
+++ dired-sync.el
@@ -5,7 +5,7 @@
 ;; Author: Sebastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, dired, rsync
 ;; Created: 2010-12-02
-;; Last changed: 2010-12-06 18:46:36
+;; Last changed: 2010-12-07 11:13:23
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -352,90 +352,93 @@ SOURCE."
 	 (src (plist-get files :src))
 	 (dst (plist-get files :dst))
 	 cmd1 cmd2)
-    (cond
-     ;; both files are remote and src cannot connect to dst
-     ((and
-       (plist-get src :host)
-       (plist-get dst :host)
-       (not (plist-get src :direct)))
-      (setq cmd1 `("ssh" "-L" 
-		   ,(format "%d:127.0.0.1:22" (plist-get dst :tunnel-port))
-		   ,(plist-get dst :host)))
-      (setq cmd2 `("ssh" "-A"  "-R"
-		   ,(format "%d:127.0.0.1:%d" (plist-get src :tunnel-port) 
-			    (plist-get dst :tunnel-port))
-		   ,(plist-get src :host)
-		   ,(concat dired-sync-bin " "
-			    (mapconcat 'concat dired-sync-args " ")
-			    (format " -e 'ssh -A -p %d " 
-				    (plist-get src :tunnel-port))
-			    "-o StrictHostKeyChecking=no "
-			    "-o UserKnownHostsFile=/dev/null' "
-			    (format "%s %s@localhost:%s" 
-				    (plist-get src :path)
-				    (plist-get dst :user) 
-				    (plist-get dst :path))))))
+    (dired-sync-with-files
+     src dst
+     (cond
+      ;; both files are remote and src cannot connect to dst
+      ((and
+	(plist-get src :host)
+	(plist-get dst :host)
+	(not (plist-get src :direct)))
+       (setq cmd1 `("ssh" "-L" 
+		    ,(format "%d:127.0.0.1:22" (plist-get dst :tunnel-port))
+		    ,(plist-get dst :host)))
+       (setq cmd2 `("ssh" "-A"  "-R"
+		    ,(format "%d:127.0.0.1:%d" (plist-get src :tunnel-port) 
+			     (plist-get dst :tunnel-port))
+		    ,(plist-get src :host)
+		    ,(concat dired-sync-bin " "
+			     (mapconcat 'concat dired-sync-args " ")
+			     (format " -e 'ssh -A -p %d " 
+				     (plist-get src :tunnel-port))
+			     "-o StrictHostKeyChecking=no "
+			     "-o UserKnownHostsFile=/dev/null' "
+			     (format "%s %s@localhost:%s" 
+				     (plist-get src :path)
+				     (plist-get dst :user) 
+				     (plist-get dst :path))))))
 
-     ;; both files are remote and src cannot connect to dst
-     ((and
-       (plist-get src :host)
-       (plist-get dst :host))
-      (setq cmd1 `("ssh" ,(plist-get src :host)
-		   ,(concat dired-sync-bin " " 
-			    (mapconcat 'concat dired-sync-args " ")
-			    " -e ssh "
-			    (shell-quote-argument (plist-get src :path))
-			    (format " %s@%s:%s" (plist-get dst :user)
-				    (plist-get dst :host)
-				    (shell-quote-argument (plist-get dst :path)))))
-	    cmd2 nil))
-     ;; one file is remote
-     ((or
-       (plist-get src :host)
-       (plist-get dst :host))
-      (setq cmd1 (apply 'append `((,dired-sync-bin)
-				  ,dired-sync-args
-				  ("-e") ("ssh")
-				  (,(if (plist-get src :host)
-					(format "%s:%s" (plist-get src :host)
-						(plist-get src :path))
-				      (plist-get src :file)))
-				  (,(if (plist-get dst :host)
-					(format "%s:%s" (plist-get dst :host)
-						(plist-get dst :path))
-				      (plist-get dst :file)))))
-	    cmd2 nil))
+      ;; both files are remote and src cannot connect to dst
+      ((and
+	(plist-get src :host)
+	(plist-get dst :host))
+       (setq cmd1 `("ssh" ,(plist-get src :host)
+		    ,(concat dired-sync-bin " " 
+			     (mapconcat 'concat dired-sync-args " ")
+			     " -e ssh "
+			     (shell-quote-argument (plist-get src :path))
+			     (format " %s@%s:%s" (plist-get dst :user)
+				     (plist-get dst :host)
+				     (shell-quote-argument (plist-get dst :path)))))
+	     cmd2 nil))
+      ;; one file is remote
+      ((or
+	(plist-get src :host)
+	(plist-get dst :host))
+       (setq cmd1 (apply 'append `((,dired-sync-bin)
+				   ,dired-sync-args
+				   ("-e") ("ssh")
+				   (,(if (plist-get src :host)
+					 (format "%s:%s" (plist-get src :host)
+						 (plist-get src :path))
+				       (plist-get src :file)))
+				   (,(if (plist-get dst :host)
+					 (format "%s:%s" (plist-get dst :host)
+						 (plist-get dst :path))
+				       (plist-get dst :file)))))
+	     cmd2 nil))
 
-     ;; all files are local
-     (t
-      (setq cmd1 (apply 'append `((,dired-sync-bin)
-				  ,dired-sync-args
-				  (,(plist-get src :file))
-				  (,(plist-get dst :file))))
-	    cmd2 nil)))
+      ;; all files are local
+      (t
+       (setq cmd1 (funcall (plist-get  dired-sync-commands :do-sync-local-local)))
+       (setq cmd2 (cadr cmd1))
+       (setq cmd1 (car cmd1))))
 
-    (let* ((p1-str (format "dired-sync %s to %s"
-			   (plist-get src :file)
-			   (plist-get dst :file)))
-	   (p1-buf (format "*%s*" p1-str))
-	   (p1 (apply 'start-process p1-str p1-buf (car cmd1) (cdr cmd1)))
-	   (p2-str (format "%s (syncing)" p1-str))
-	   (p2-buf (format "*%s*" p2-str))
-	   p2)
 
-      (process-put p1 :buf p1-buf)
-      (unless cmd2
-	(set-process-sentinel p1 'dired-sync-proc-sentinel))
-      (when cmd2
-	;;make sur shh tunnel is up
-	(sit-for dired-sync-command-delay)
-	(setq p2 (apply 'start-process p2-str p2-buf (car cmd2) (cdr cmd2)))
-	(process-put p2 :related p1)
-	(process-put p2 :buf p2-buf)
-	(set-process-sentinel p2 'dired-sync-proc-sentinel)))
-    ;;(message (concat "C1: " (mapconcat 'append cmd1 " "))))
-    ;;(message (concat "C2: " (mapconcat 'append cmd2 " "))))
-    t))
+     (message (concat "C1: " (mapconcat 'append cmd1 " ")))
+     (message (concat "C2: " (mapconcat 'append cmd2 " ")))
+
+     (let* ((p1-str (format "dired-sync %s to %s"
+			    (plist-get src :file)
+			    (plist-get dst :file)))
+	    (p1-buf (format "*%s*" p1-str))
+	    (p1 (apply 'start-process p1-str p1-buf (car cmd1) (cdr cmd1)))
+	    (p2-str (format "%s (syncing)" p1-str))
+	    (p2-buf (format "*%s*" p2-str))
+	    p2)
+
+       (process-put p1 :buf p1-buf)
+       (unless cmd2
+	 (set-process-sentinel p1 'dired-sync-proc-sentinel))
+       (when cmd2
+	 ;;make sur shh tunnel is up
+	 (sit-for dired-sync-command-delay)
+	 (setq p2 (apply 'start-process p2-str p2-buf (car cmd2) (cdr cmd2)))
+	 (process-put p2 :related p1)
+	 (process-put p2 :buf p2-buf)
+	 (set-process-sentinel p2 'dired-sync-proc-sentinel)))
+
+     t)))
 
 
 (defun dired-sync-proc-sentinel (proc change)
